d6cdd744c028bbfc42086e0ae2839a9168dabd27
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-05-14 21:56:36 +0800

    dropdowns and carousel

--- components/PartnersDropdown.vue
+++ components/PartnersDropdown.vue
@@ -1,13 +1,30 @@
 <template v-slot:default="{ isActive }">
-  <v-card title="Partners" style="border-radius: 0px">
+  <v-card
+    title="Partners"
+    style="border-radius: 0px"
+    class="max-h-72 pb-4 pl-5 font-Poppins"
+  >
+    <!-- can only fit up to 4 items, trying to fix it could take time -->
     <v-list lines="one">
       <v-list-item
-        v-for="n in 3"
-        :key="n"
-        :title="'Item ' + n"
-        subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+        v-for="partner in partners"
+        :title="partner.title"
+        :subtitle="partner.subtitle"
         class="text-hover"
       ></v-list-item>
     </v-list>
   </v-card>
 </template>
+
+<script setup>
+const partners = ref([
+  {
+    title: "DataStax",
+    subtitle: "DataStax Subtitle",
+  },
+  {
+    title: "Fastly",
+    subtitle: "Fastly Subtitle",
+  },
+]);
+</script>
--- components/ServicesDropdown.vue
+++ components/ServicesDropdown.vue
@@ -1,13 +1,38 @@
 <template v-slot:default="{ isActive }">
-    <v-card title="Services" style="border-radius: 0px">
-      <v-list lines="one">
-        <v-list-item
-          v-for="n in 3"
-          :key="n"
-          :title="'Item ' + n"
-          subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit mimiimimi"
-          class="text-hover"
-        ></v-list-item>
-      </v-list>
-    </v-card>
-  </template>
+  <v-card
+    title="Services"
+    style="border-radius: 0px"
+    class="max-h-72 pb-4 pl-5 font-Poppins"
+  >
+    <!-- can only fit up to 4 items, trying to fix it could take time -->
+    <v-list lines="one">
+      <v-list-item
+        v-for="service in services"
+        :title="service.title"
+        :subtitle="service.subtitle"
+        class="text-hover"
+      ></v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script setup>
+const services = ref([
+  {
+    title: "Service 1",
+    subtitle: "Service 1 Subtitle",
+  },
+  {
+    title: "Service 2",
+    subtitle: "Service 2 Subtitle",
+  },
+  {
+    title: "Service 2",
+    subtitle: "Service 2 Subtitle",
+  },
+  {
+    title: "Service 2",
+    subtitle: "Service 2 Subtitle",
+  },
+]);
+</script>
