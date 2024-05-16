c2677a276c75b6f8b76a80d6983f64e2d9984f9d
Ulvintier <joseph32302@yahoo.com>
2024-05-16 10:53:39 +0800

    Service page DONE
    Page design of service page is done
    The Custom Software Development products/projects needs descriptions in the carousel.

--- pages/ServiceCS.vue
+++ pages/ServiceCS.vue
@@ -1,80 +1,83 @@
 <template>
 
-    <div class="body-service">
+    <div class="Service-body">
 
             <ServicesSidebar />
 
-        <div class="body-main-content" >
+        <div class="Service-body-maincontent" >
 
 
-                    <p class="text-center ServiceTitle"> Custom Software Development</p>
+                    <div class="ServiceHeader">
 
-                    <p class="text-justify-center text-center ServiceSubtext">
-                        We specialize in building and optimizing software solutions tailored to unique business and organizational objectives and processes. 
-                        Through close collaboration throughout the development process, we ensure continuous alignment with business and operational goals. 
-                        Our dedication is focused on delivering exceptional software solutions for every client.
-                    </p>
+
+                        <div class="Service-Title">
+
+                            CONSULTING SERVICE
+
+                        </div>
+
+                        <div class="Service-Intro">
+
+                            Our consulting service offers advanced software development solutions powered by Fastly and DataStax. 
+                            With our expertise in custom software development, performance optimization, and security, we help 
+                            businesses build scalable, reliable, and secure applications that drive growth and innovation. 
+                            Partner with us to unlock the full potential of your software development projects.
+
+                        </div>
+
+
+                    </div>
 
         
 
-            <div class="body-content text-justify">
+            <div class="Service-body-content text-justify">
 
-                <div class="ServiceDescription">
+                <div class="Parag-Number">
+                    <p class="PN01" style="margin-top: 20%;">01</p> <br />
+                    <p class="PN02" >02</p> <br />
+                    <p class="PN03" style="opacity: 0;">03</p> <br />
+                </div>
 
-                    <div class="SNumbering">
+                <div class="Description-Wrapped">
 
-                        <p style="color:#FF5C01; font-size:25px; font-weight: 600;" >
-                            01
-                        </p>
 
-                        <p>
-                            At our core, we are dedicated to crafting software solutions that are not only tailored to your unique business needs but also optimized to drive your organization towards its objectives. 
-                            Our approach begins with a deep understanding of your business and operational processes, allowing us to design solutions that seamlessly integrate with your existing workflows and infrastructure.
-                        </p>
+                <div class="BlackRibbon"></div>
+
+
+                <div class="Wrapped-contents">
+
+                <div class="Service-Description text-justify">
+
+                    <p class="SD01">
+                        Our consulting service specializes in advanced software development solutions tailored to the unique needs of modern businesses. 
+                        Leveraging cutting-edge technologies from Fastly and DataStax, we deliver comprehensive services aimed at achieving business 
+                        goals with speed, reliability, and scalability.
+                    </p>
+
+                    <p class="SD02">
+                        With Fastly's edge cloud platform integrated into our solutions, we accelerate content delivery and enhance user experience. 
+                        Leveraging Fastly's global network, we ensure swift and dependable access to web content, driving improved customer satisfaction 
+                        and engagement.
+                    </p>
+
+                    <p class="SD03">
+                        Our team of experienced consultants brings deep expertise in DataStax's database management solutions, including Apache Cassandra. 
+                        Utilizing DataStax's distributed database technology, we build scalable and resilient applications capable of handling massive 
+                        data volumes effortlessly.
+                    </p>
+
+                </div>
+
+                    <div class="CSimage"></div>
                     
-                    </div>
-
-                    <div class="SNumbering">
-
-                    <p style="color:#FF5C01; font-size:25px; font-weight: 600;">
-                        02
-                    </p>
-
-                    <p>
-                        Throughout the development process, we prioritize collaboration and communication, working closely with you to ensure that every aspect of the software aligns with your goals and objectives. 
-                        This continuous dialogue enables us to adapt and refine the solution in real-time, ensuring that it evolves in line with your evolving needs.
-                    </p>
-
-                    </div>
-
-
-                    <div class="SNumbering">
-
-                    <p style="color:#FF5C01; font-size:25px; font-weight: 600;">
-                        03
-                    </p>
-
-                    <p>
-                        With our team of experienced professionals by your side, your software needs are in capable hands. Our passion lies in leveraging technology to drive business success, 
-                        and we are excited about the opportunity to collaborate with businesses on their journey towards growth and innovation.
-                    </p>
-
-                    </div>
+                </div>
 
                 </div>
 
-                <div class="CContainer">
-
-                    <ServiceCarousel style="margin-left: 5%;" />
-
-                </div>
-          
 
             </div>
 
          
-
-
         </div>
      
 
@@ -82,137 +85,8 @@
 
 </template>
 
-<style>
+<style scoped>
 
-    .body-service {
-
-        background-image: url(/assets/images/servicesBG.png);
-        background-size: cover;
-        margin: 0px;
-        height: 100vh;
-        width: 100%;
-        color: rgb(255, 255, 255);
-        overflow-y: hidden;
-        
-    }
-
-    .body-main-content {
-
-        position: relative;
-        height: 100vh;
-        width: 80%;
-        overflow-y: hidden;
-        display: flex;
-        flex-direction: column;
-        
-
-    }
-
-
-    .ServiceTitle {
-
-        font-size: 55px;
-        color: #FF5C01;
-        margin-top: 2%;
-        font-weight: 500;
-
-    }
-
-    .ServiceSubtext {
-
-        word-wrap: break-word;
-        overflow-wrap: break-word;
-        font-size: 20px;
-        margin: auto 2% 5% 2%;
-        padding: 0% 5% 0% 5%;
-        font-weight: 500;
-
-    }
-
-    .body-content {
-
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-gap: 2px;
-        margin: auto auto 0% 3%;
-        padding: 2% 3% 2% 3%;
-        height: 100%;
-
-        background-color: #37373788;
-        border-radius: 10px 10px 0px 0px;
-        box-shadow: rgba(44, 25, 6, 0.4) 0px 0px 0px 2px, 
-                    rgba(44, 30, 6, 0.65) 0px 4px 6px -1px, 
-                    rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
-
-    }
-
-    .ServiceDescription {
-
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-gap: 2px;
-        margin: 2% 0% 0% 0%;
-
-    }
-
-    .CContainer {
-
-        position: absolute;
-        top: 30%;
-        left: calc(60% + 5%);
-        overflow-x: auto hidden;
-
-    }
-
-    .SNumbering p {
-
-        margin: 0% 0% 2% 10%;
-        font-size: 11px;
-
-    }
+    @import url(/assets/css/ServicePage.css);
     
-
-    @media (max-width: 768px) {
-        .Sidebar {
-            display: none;
-        }
-        .body-main-content {
-            width: 100%;
-        }
-        .ServiceTitle {
-            font-size: 30px;
-            margin-bottom: 10%;
-        }
-        .ServiceSubtext {
-            font-size: 12px;
-            margin: 0% 0% 15% 0%;
-        }
-        .body-service {
-            background-image: url(/assets/images/mobile-bg.png);
-            background-size: cover;
-        }
-        .body-content {
-            margin: auto 3% 0% 3%;
-            font-size: 12px;
-        }
-        .carousel {
-            margin-top: 2%;
-        }
-        .CCard {
-            max-width: 300;
-        }
-        .SNumbering p {
-            font-size: 8px;
-        }
-        .CContainer {
-            top: 37%;
-            left: calc(50% + 5%);
-        }
-        .ServiceDescription {
-            display: block;
-            grid-template-columns: unset;
-            grid-gap: unset;
-        }
-    }
-
 </style>
--- pages/ServiceCSD.vue
+++ pages/ServiceCSD.vue
@@ -32,9 +32,9 @@
             <div class="Service-body-content text-justify">
 
                 <div class="Parag-Number">
-                    <p style="margin-bottom: 57%;">01</p> <br />
-                    <p style="margin-bottom: 57%;">02</p> <br />
-                    <p>03</p> <br />
+                    <p class="PN01" style="margin-bottom: 80%;">01</p> <br />
+                    <p class="PN02" style="margin-bottom: 60%;">02</p> <br />
+                    <p class="PN03" style="opacity: 0;">03</p> <br />
                 </div>
 
                 <div class="Description-Wrapped">
@@ -49,20 +49,20 @@
 
                 <div class="Service-Description text-justify">
 
-                    <p style="opacity: 0; animation: slideFromLeft 1s ease-in-out forwards; animation-delay: 3.4s;">
+                    <p class="SD01">
                         At our core, we are dedicated to crafting software solutions that are not only tailored to your unique 
                         business needs but also optimized to drive your organization towards its objectives. Our approach begins 
                         with a deep understanding of your business and operational processes, allowing us to design solutions that 
                         seamlessly integrate with your existing workflows and infrastructure.
                     </p>
 
-                    <p style="opacity: 0; animation: slideFromLeft 1s ease-in-out forwards; animation-delay: 3.5s;">
+                    <p class="SD02">
                         Throughout the development process, we prioritize collaboration and communication, working closely with you 
                         to ensure that every aspect of the software aligns with your goals and objectives. This continuous dialogue 
                         enables us to adapt and refine the solution in real-time, ensuring that it evolves in line with your evolving needs.
                     </p>
 
-                    <p style="opacity: 0; animation: slideFromLeft 1s ease-in-out forwards; animation-delay: 3.6s;">
+                    <p class="SD03">
                         With our team of experienced professionals by your side, your software needs are in capable hands. Our passion 
                         lies in leveraging technology to drive business success, and we are excited about the opportunity to collaborate 
                         with businesses on their journey towards growth and innovation.
@@ -72,44 +72,30 @@
 
                     <div class="Service-Card">
 
-                        <ServiceCarousel  
-
-                            style="
-                                animation: slideFromBottom 1s ease-in-out forwards;
-                                animation-delay: 4s;
-                                opacity: 0;
-                            "
-                        
-                        />
+                        <ServiceCarousel class="Service-Carousel"/>
 
                     </div>
 
                     <div class="Past-Projects">
 
-                        <v-carousel :show-arrows="false" hide-delimiters cycle>
+                        <v-carousel :show-arrows="false" hide-delimiters cycle @change="updateCurrentIndex">
                             <v-carousel-item v-for="(slide, i) in slides" :key="i">
                                 <div class="text-h2">{{ slide }}</div>
                                     <p style="color: white; margin: 10% 0% 0% 10%;"> {{ texts[i] }} </p>
                             </v-carousel-item>
+
                         </v-carousel>
 
                     </div>
                     
-
                 </div>
 
                 </div>
 
-              
-
-                
-          
 
             </div>
 
          
-
-
         </div>
      
 
@@ -142,215 +128,8 @@
 
 </script>
 
-<style>
+<style scoped>
 
-    .Service-body {
-
-        display: flex;
-        margin: 0px;
-        height: 100vh;
-        width: 100%;
-        color: rgb(255, 255, 255);
-        overflow-y: hidden;
-        
-    }
-
-    .Service-body-maincontent {
-
-        position: relative;
-        height: 100vh;
-        width: 100%;
-        overflow-y: hidden;
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
-        
-    }
-
-    .ServiceHeader {
-
-        background-color: #F5D7BB;
-        height: 45%;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-    }
-
-    .Service-Title {
-
-        text-align: center;
-        color: #FF5C01;
-        font-size: 45px;
-        font-weight: 500;
-        margin: 1%;
-
-    }
-
-    .Service-Intro {
-
-        font-size: 15px;
-        text-transform: uppercase;
-        color: black;
-        text-align: center;
-        font-weight: 500;
-
-        background-color: white;
-        margin: 2%;
-        padding: 1%;
-        border-radius: 15px;
-        width: 70%;
-        align-items: center;
-
-    }
-
-    .Service-body-content {
-
-        background-color: black;
-        flex-grow: 1;
-        width: 100%;
-        position: relative;
-        display: flex;
-
-    }
+    @import url(/assets/css/ServicePage.css);
     
-    .Parag-Number {
-
-        margin-top: 1%;
-        height: 100%;
-        width: 5%;
-        border-style: solid;
-        border-color: #FF5C01;
-        border-radius: 15px;
-        border-width: 3px;
-        background-color: black;
-        z-index: 2;
-        padding-top: 3%;
-
-        color: #FF5C01;
-        font-size: 38px;
-        text-align: center;
-        font-weight: 600;
-
-        animation: slideFromBottom 1s ease-in-out forwards;
-
-    }
-
-    @keyframes slideFromBottom {
-        0% 
-            {
-                transform: translateY(100%);
-                opacity: 0;
-            }
-        100% 
-            {
-                transform: translateY(0%);
-                opacity: 1;
-            }
-    }
-
-    .BlackRibbon {
-
-        background-color: #222222;
-        height: 12%;
-        width: 100%;
-        margin-top: 1.7%;
-        z-index: 1;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: #FF5C01;
-        font-size: 20px;
-        font-weight: 500;
-        top: calc(50% - 10%);
-        right: 0;
-
-        animation: slideFromLeft 2s ease-in-out forwards;
-        animation-delay: 1s;
-        opacity: 0;
-
-    }
-
-    .BlackRibbon p {
-
-        animation: fadein 2s ease-in-out forwards;
-        animation-delay: 4s;
-        opacity: 0;
-
-    }
-
-    @keyframes slideFromLeft {
-        0% 
-            {
-                transform: translateX(-100%);
-                opacity: 1;
-            }
-        100% 
-            {
-                transform: translateX(0%);
-                opacity: 1;
-            }
-    }
-
-    @keyframes fadein {
-        0% 
-            {
-                opacity: 0;
-            }
-        100% 
-            {
-                opacity: 1;
-            }
-    }
-
-    .Description-Wrapped {
-
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-
-    }
-
-    .Wrapped-contents {
-
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-
-    }
-
-    .Service-Description {
-
-        font-size: 13px;
-        color: white;
-        width: 40%;
-        
-
-    }
-
-    .Service-Description p {
-
-        margin: 2% 0% 8% 2%;
-
-    }
-
-    .Service-Card {
-
-        height: 100%;
-        width: 27%;
-        margin: 1% 0% 2% 2%; 
-
-    }
-
-    .Past-Projects {
-
-        margin: 2%;
-        color: #FF5C01;
-        width: 50%;
-
-    }
-   
-
 </style>
