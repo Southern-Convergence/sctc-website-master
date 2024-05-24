2f9eeb5ddd066ec75175c234ce6562ef3d5765c3
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-05-24 15:19:36 +0800

    tried to dynamically change pages

---
+++ components/InPartnerContent.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="content-animation h-full">
+    <!-- the content reaching the bottom of the page is intentional -->
+    <div class="w-11/12" style="margin: 0px auto">
+      <v-tabs
+        v-model="tab"
+        grow
+        hide-slider
+        mandatory="force"
+        color="grey-darken-2"
+        class="font-Roboto"
+        density="comfortable"
+        :mobile="false"
+      >
+        <v-tab
+          text="Description"
+          value="description"
+          class="rounded-t-xl w-1/2"
+          :class="tab === 'description' ? 'bg-white' : getInactiveClass(route.name)"
+          style="font-size: 12px"
+        ></v-tab>
+        <v-tab
+          text="Products"
+          value="products"
+          class="rounded-t-xl w-1/2"
+          :class="tab === 'products' ? 'bg-white' : getInactiveClass(route.name)"
+          style="font-size: 12px"
+        ></v-tab>
+      </v-tabs>
+    </div>
+
+    <div class="networkBG w-11/12 bg-white h-full" style="margin: 0px auto">
+      <v-window v-model="tab" direction="horizontal">
+        <v-window-item value="description">
+          <div
+            class="p-5 md:p-20"
+            style="margin: 0px auto; background-color: rgba(255, 255, 255, 0.5); padding-top: 1.5rem"
+          >
+            <h1
+              class="font-black font-Roboto text-center text-lg md:text-xl xl:text-2xl 2xl:text-4xl text-purple-900"
+              style="text-transform: capitalize"
+              :class="getTitleColor(route.name)"
+            >
+              {{ route.name }}
+            </h1>
+            <v-divider
+              thickness="5"
+              color="black"
+              length="80"
+              class="border-opacity-100 p-0"
+              style="margin: 10px auto 20px auto"
+            >
+            </v-divider>
+
+            <p
+              class="mb-8 text-xs md:text-sm lg:text-sm xl:text-sm 2xl:text-base font-Poppins"
+              v-for="desc in description"
+            >
+              {{ desc }}
+            </p>
+          </div>
+        </v-window-item>
+        <v-window-item value="products">
+          <div class="p-5 md:p-8" style="margin: 0px auto; padding-top: 1.5rem">
+            <h1
+              class="font-black font-Roboto text-center text-lg md:text-xl xl:text-2xl 2xl:text-4xl text-purple-900"
+              :class="getTitleColor(route.name)"
+            >
+              Products
+            </h1>
+            <v-divider
+              thickness="5"
+              color="black"
+              length="80"
+              class="border-opacity-100 p-0"
+              style="margin: 15px auto 30px auto"
+            >
+            </v-divider>
+
+            <!-- PRODUCTS WEB VIEW -->
+            <div class="grid hidden-sm-and-down" :class="getProductsCount()">
+              <!-- copying what's in the current website -->
+              <v-card flat class="mx-4 xl:mx-3" v-for="product in products">
+                <v-img :src="`/static/images/${product.image}`"></v-img>
+                <v-card-text class="font-Poppins" style="padding: 20px 0px">
+                  <!-- md:text-xs 2xl:text-base -->
+                  {{ product.text }}
+                </v-card-text>
+              </v-card>
+            </div>
+
+            <!-- PRODUCTS MOBILE VIEW -->
+            <div class="grid hidden-md-and-up">
+              <!-- copying what's in the current website -->
+              <v-carousel cycle hide-delimiters touch show-arrows="hover" progress="grey-darken-1">
+                <v-carousel-item v-for="product in products">
+                  <v-card flat class="mx-4 xl:mx-3">
+                    <v-img :src="`/static/images/${product.image}`"></v-img>
+                    <v-card-text class="text-xs font-Poppins" style="padding: 20px 0px">
+                      {{ product.text }}
+                    </v-card-text>
+                  </v-card>
+                </v-carousel-item>
+              </v-carousel>
+            </div>
+          </div>
+        </v-window-item>
+      </v-window>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+let tab = ref('');
+
+const props = defineProps({
+  products: {
+    type: Array,
+    default: () => [],
+  },
+  description: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const getInactiveClass = (partner) => {
+  let pageColor = '';
+  switch (partner) {
+    case 'DataStax':
+      pageColor = 'inactiveDataStax';
+      break;
+    case 'fastly':
+      pageColor = 'inactiveFastly';
+      break;
+  }
+  return pageColor;
+};
+
+const getTitleColor = (partner) => {
+  let titleColor = '';
+  switch (partner) {
+    case 'DataStax':
+      titleColor = 'text-purple-900';
+      break;
+    case 'fastly':
+      titleColor = 'text-red-900';
+      break;
+  }
+  return titleColor;
+};
+
+const getProductsCount = () => {
+  console.log(props.products.length);
+  let columns = 'md:grid-cols-' + props.products.length + '';
+  return columns.toString();
+};
+
+console.log(getProductsCount());
+</script>
+
+<style scoped>
+.inactiveDataStax {
+  background-color: #e1bee724;
+  border-top: 2px #e1bee7b1 solid;
+  border-right: 2px #e1bee7b1 solid;
+  border-left: 2px #e1bee7b1 solid;
+  color: #e1bee7;
+}
+
+.inactiveFastly {
+  background-color: #e7bebe24;
+  border-top: 2px #e7bebeb1 solid;
+  border-right: 2px #e7bebeb1 solid;
+  border-left: 2px #e7bebeb1 solid;
+  color: #e7bebe;
+}
+
+.networkBG {
+  background-image: url(/assets/images/networkbg-transparent2.png);
+  background-size: auto 100%;
+}
+
+.content-animation {
+  animation: opacity-change 0.8s ease;
+}
+
+@keyframes opacity-change {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 100;
+  }
+}
+</style>
--- pages/fastly.vue
+++ pages/fastly.vue
@@ -2,145 +2,44 @@
   <div class="gradient h-full w-full text-white">
     <img
       src="/assets/images/fastly.png"
-      class="header aspect-auto border-8 invert p-3"
-      style="margin: 0px auto 15px auto"
+      class="header aspect-auto border-8 invert scale-75"
+      style="margin: 0px auto 15px auto;"
     />
 
-    <div class="content-animation h-full">
-      <!-- the content reaching the bottom of the page is intentional -->
-      <div class="w-11/12" style="margin: 0px auto">
-        <v-tabs
-          v-model="tab"
-          grow
-          hide-slider
-          mandatory="force"
-          color="red-darken-3"
-          class="font-Roboto"
-          style="height: 50px"
-        >
-          <v-tab
-            text="Description"
-            value="description"
-            class="rounded-t-xl w-1/2"
-            :class="tab === 'description' ? 'bg-white' : 'inactive'"
-            style="height: 50px; font-size: 14px"
-          ></v-tab>
-          <v-tab
-            text="Products"
-            value="products"
-            class="rounded-t-xl w-1/2"
-            :class="tab === 'products' ? 'bg-white' : 'inactive'"
-            style="height: 50px; font-size: 14px"
-          ></v-tab>
-        </v-tabs>
-      </div>
-
-      <div class="networkBG w-11/12 bg-white h-full" style="margin: 0px auto">
-        <v-window v-model="tab" direction="horizontal">
-          <v-window-item value="description">
-            <div
-              class="text-center text-sm md:text-sm lg:text-base xl:text-lg 2xl:text-xl p-5 md:p-10 font-Poppins"
-              style="background-color: rgba(255, 255, 255, 0.5)"
-            >
-              <h1 class="font-black font-Roboto text-center text-lg md:text-xl xl:text-2xl 2xl:text-4xl text-red-900">
-                Fastly
-              </h1>
-              <v-divider
-                thickness="5"
-                color="black"
-                length="80"
-                class="border-opacity-100 p-0"
-                style="margin: 15px auto 30px auto"
-              >
-              </v-divider>
-
-              <!-- to fill up the description page -->
-              <p class="mb-8">
-                We have partnered with Fastly to give your website lightning-fast delivery and unparalleled reliability.
-              </p>
-              <p class="mb-8">
-                Your website's technology is the backbone of your user experience, and it's a key driver of your
-                success. With the right infrastructure, you can scale intelligently, enhance security, and optimize user
-                engagement. And with Fastly handling the heavy lifting, you can focus on what truly matters for your
-                business.
-              </p>
-              <p class="mb-8">
-                Fastly’s powerful edge cloud platform empowers companies to run, secure, and deliver their website
-                content and applications to create remarkable user experiences at global scale.
-              </p>
-            </div>
-          </v-window-item>
-          <v-window-item value="products">
-            <div class="p-5 md:p-10" style="margin: 0px auto">
-              <h1 class="font-black font-Roboto text-center text-lg md:text-xl xl:text-2xl 2xl:text-4xl text-red-900">
-                Products
-              </h1>
-              <v-divider
-                thickness="5"
-                color="black"
-                length="80"
-                class="border-opacity-100 p-0"
-                style="margin: 15px auto 30px auto"
-              >
-              </v-divider>
-
-              <!-- PRODUCTS WEB VIEW -->
-              <div
-                class="grid grid-cols-1 md:grid-cols-3 scroll-auto hidden-sm-and-down"
-                style="overflow: auto !important"
-              >
-                <!-- copying what's in the current website -->
-                <v-card flat class="mx-4 xl:mx-3" v-for="product in products">
-                  <v-img :src="`/static/images/${product.image}`"></v-img>
-                  <v-card-text class="text-xs md:text-xs 2xl:text-base font-Poppins" style="padding: 20px 0px">
-                    {{ product.text }}
-                  </v-card-text>
-                </v-card>
-              </div>
-
-              <!-- PRODUCTS MOBILE VIEW -->
-              <div class="grid grid-cols-1 scroll-auto hidden-md-and-up" style="overflow: auto !important">
-                <!-- copying what's in the current website -->
-                <v-carousel cycle hide-delimiters touch show-arrows="hover" progress="red-darken-3">
-                  <v-carousel-item v-for="product in products">
-                    <v-card flat class="mx-4 xl:mx-3">
-                      <v-img :src="`/static/images/${product.image}`"></v-img>
-                      <v-card-text class="text-xs md:text-sm lg:text-base font-Poppins" style="padding: 20px 0px">
-                        {{ product.text }}
-                      </v-card-text>
-                    </v-card>
-                  </v-carousel-item>
-                </v-carousel>
-              </div>
-            </div>
-          </v-window-item>
-        </v-window>
-      </div>
-    </div>
+    <InPartnerContent :products="products" :description="description" />
   </div>
 </template>
 
 <script setup>
+import InPartnerContent from '../components/InPartnerContent.vue';
 import { ref } from 'vue';
 
 useHead({
   title: 'Fastly',
 });
 
-let tab = ref('');
+const description = ref([
+  "We're thrilled to introduce our partnership with Fastly – the industry leader in content delivery, security, and edge computing. Together, we're committed to helping businesses like yours achieve unparalleled digital performance and security, all while delivering exceptional user experiences.",
+  "Fastly's Content Delivery Network (CDN) is the backbone of modern internet delivery. With strategically positioned edge servers worldwide, Fastly ensures lightning-fast access to web content for users across the globe. Through our partnership, you gain access to this powerful CDN, enabling you to reach your audience faster and more reliably than ever before.",
+  "Security is paramount in today's digital landscape, and Fastly's advanced security features provide peace of mind for businesses of all sizes. From DDoS protection to real-time threat intelligence, Fastly's comprehensive security suite safeguards your digital assets against evolving cyber threats, ensuring the integrity and availability of your online presence.",
+]);
 
 const products = ref([
   {
-    image: 'vector-search.png',
-    text: 'Say goodbye to expensive bandwidth and storage costs, and ease the burden on your servers or central cloud. With Fastly, you can effortlessly offload content and streamline your operations, so you can focus on what truly drives your business forward.',
+    image: 'CDN.png',
+    text: "Fastly's Content Delivery Network (CDN) revolutionizes the way businesses deliver online content to their audiences. With strategically positioned edge servers worldwide, Fastly's CDN ensures lightning-fast access to web content, optimizing user experiences regardless of location. Discover how Fastly's CDN empowers businesses to reach global audiences with unparalleled speed and reliability.",
   },
   {
-    image: 'vector-search.png',
-    text: "Secure your customers' data, safeguard your brand's integrity, and instill trust in your users by displaying HTTPS sites across all your properties. With Fastly, you can easily protect your website and enhance your reputation, so you can focus on delivering the best possible user",
+    image: 'computeedge.png',
+    text: "Fastly's Compute@Edge is reshaping the digital landscape by bringing computation closer to the user, enabling real-time processing and customization of content at the edge of the network. Experience the power of Compute@Edge as it unlocks endless possibilities for dynamic, personalized online experiences, setting new standards for speed, flexibility, and innovation.",
   },
   {
-    image: 'astra-streaming.png',
-    text: 'Streamline your workflow and empower your teams to achieve more, all while seamlessly syncing with the tools they need to succeed. With Fastly, you can accelerate your operations and enhance productivity, so you can focus on taking your business to the next level. ',
+    image: 'security.png',
+    text: "Fastly is dedicated to ensuring the safety and integrity of your digital assets in an ever-evolving cyber landscape. Our comprehensive security solutions offer protection against a wide range of threats, from DDoS attacks to data breaches. With real-time threat intelligence and robust security features, Fastly provides the peace of mind you need to focus on growing your business while we safeguard your online presence. Explore how Fastly's security solutions can elevate your defenses and protect your brand in today's digital world.",
+  },
+  {
+    image: 'next-gen_waf.png',
+    text: "Fastly's Next-Gen Web Application Firewall (WAF) is your frontline defense against sophisticated cyber threats targeting your web applications. With advanced threat detection capabilities and real-time updates, our WAF goes beyond traditional security measures to provide proactive protection for your online assets. Explore how Fastly's Next-Gen WAF empowers businesses to confidently navigate the digital landscape, ensuring the integrity and availability of their web applications.",
   },
 ]);
 </script>
@@ -154,41 +53,16 @@ const products = ref([
   animation: emphasis 0.8s ease;
 }
 
-.inactive {
-  background-color: #e1bee724;
-  border-top: 2px #e7bebeb1 solid;
-  border-right: 2px #e7bebeb1 solid;
-  border-left: 2px #e7bebeb1 solid;
-  color: #e1bee7;
-}
-
-.networkBG {
-  background-image: url(/assets/images/networkbg-transparent2.png);
-  background-size: auto 100%;
-}
-
-.content-animation {
-  animation: opacity-change 0.8s ease;
-}
-
 @keyframes emphasis {
   0% {
-    transform: scale(0.8); /* Adjust the scale factor as needed */
+    transform: scale(0.75); /* Adjust the scale factor as needed */
   }
   50% {
-    transform: scale(1.1); /* Adjust the scale factor as needed */
+    transform: scale(.85); /* Adjust the scale factor as needed */
   }
   100% {
-    transform: scale(1); /* Adjust the scale factor as needed */
+    transform: scale(.75); /* Adjust the scale factor as needed */
   }
 }
 
-@keyframes opacity-change {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 100;
-  }
-}
 </style>
