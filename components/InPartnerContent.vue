c81ad5b3d51f595372ca7f7e2cbb347a3d82b02c
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-06-03 16:30:48 +0800

    new format for partners page

--- components/InPartnerContent.vue
+++ components/InPartnerContent.vue
@@ -1,113 +1,166 @@
 <template>
   <div class="slide-animation h-full">
     <!-- the content reaching the bottom of the page is intentional -->
-    <div class="w-10/12" style="margin: 0px auto">
-      <v-tabs
-        v-model="tab"
-        grow
-        hide-slider
-        mandatory="force"
-        color="grey-darken-2"
-        class="font-Roboto"
-        density="compact"
-        :mobile="false"
-      >
-        <v-tab
-          text="Description"
-          value="description"
-          class="rounded-t-xl w-1/2"
-          :class="tab === 'description' ? 'bg-white' : getInactiveClass(route.name)"
-          style="font-size: 12px"
-        ></v-tab>
-        <v-tab
-          text="Products"
-          value="products"
-          class="rounded-t-xl w-1/2"
-          :class="tab === 'products' ? 'bg-white' : getInactiveClass(route.name)"
-          style="font-size: 12px"
-        ></v-tab>
-      </v-tabs>
-    </div>
+    <div class="networkBG w-12/12 bg-white h-full d-flex flex-row" style="margin: 0px auto">
+      <div class="hidden-sm-and-up p-2">
+        <v-menu
+          width="100%"
+          transition="slide-x-transition"
+          class="font-Poppins"
+          location="center"
+          style="margin: 0px !important; padding: 0px !important"
+        >
+          <template v-slot:activator="{ props }">
+            <v-btn icon v-bind="props">
+              <v-icon>mdi-menu-right</v-icon>
+            </v-btn>
+          </template>
+          <v-list style="" width="">
+            <v-list-item>
+              <v-tabs
+                v-model="tab"
+                grow
+                hide-slider
+                mandatory="force"
+                class="font-Roboto"
+                density="compact"
+                :mobile="false"
+                direction="vertical"
+              >
+                <v-tab
+                  text="Description"
+                  value="description"
+                  :class="tab === 'description' ? getActiveClass(route.name) : 'bg-white'"
+                  style="font-size: 12px"
+                ></v-tab>
+                <v-tab
+                  text="Products"
+                  value="products"
+                  :class="tab === 'products' ? getActiveClass(route.name) : 'bg-white'"
+                  style="font-size: 12px"
+                ></v-tab>
+              </v-tabs>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
 
-    <div class="networkBG w-10/12 bg-white h-full" style="margin: 0px auto">
-      <v-window v-model="tab" direction="horizontal">
-        <v-window-item value="description">
-          <div class="p-5 pt-md-5 w-12/12 md:w-11/12" style=" margin: 0px auto; background-color: rgba(255, 255, 255, 0.5);">
-            <h1
-              class="font-black font-Roboto text-center text-xl md:text-xl xl:text-2xl 2xl:text-4xl text-purple-900 pt-md-5"
-              :class="getTitleColor(route.name)"
-            >
-              {{ tagline }}
-            </h1>
-            <v-divider
-              thickness="5"
-              color="black"
-              length="80"
-              class="border-opacity-100 p-0"
-              style="margin: 10px auto 20px auto"
-            >
-            </v-divider>
+      <v-row class="">
+        <v-col cols="2" class="hidden-md-and-down">
+          <v-tabs
+            v-model="tab"
+            grow
+            hide-slider
+            mandatory="force"
+            class="font-Roboto"
+            density="compact"
+            :mobile="false"
+            direction="vertical"
+          >
+            <v-tab
+              text="Description"
+              value="description"
+              :class="tab === 'description' ? getActiveClass(route.name) : 'bg-white'"
+              style="font-size: 12px"
+            ></v-tab>
+            <v-tab
+              text="Products"
+              value="products"
+              :class="tab === 'products' ? getActiveClass(route.name) : 'bg-white'"
+              style="font-size: 12px"
+            ></v-tab>
+          </v-tabs>
+        </v-col>
+        <v-col class="overflow-hidden">
+          <v-window v-model="tab" direction="vertical" class="">
+            <v-window-item value="description">
+              <div
+                class="w-11/12 p-5 md:p-8"
+                style="margin: 0px auto; background-color: rgba(255, 255, 255, 0.5); padding-top: 1.5rem"
+              >
+                <h1
+                  class="font-black font-Roboto text-center text-xl md:text-xl xl:text-2xl 2xl:text-4xl text-purple-900"
+                  :class="getTitleColor(route.name)"
+                >
+                  {{ tagline }}
+                </h1>
+                <v-divider
+                  thickness="5"
+                  color="black"
+                  length="80"
+                  class="border-opacity-100 p-0"
+                  style="margin: 10px auto 20px auto"
+                >
+                </v-divider>
 
-            <p class="mb-4 md:mb-8 text-xs md:text-sm xl:text-md 2xl:text-lg font-Poppins" v-for="desc in description">
-              {{ desc }}
-            </p>
-          </div>
-        </v-window-item>
-        <v-window-item value="products">
-          <div class="p-5 md:p-8" style="margin: 0px auto; padding-top: 1.5rem">
-            <h1
-              class="font-black font-Roboto text-center text-lg md:text-xl xl:text-2xl 2xl:text-4xl text-purple-900"
-              :class="getTitleColor(route.name)"
-            >
-              Products
-            </h1>
-            <v-divider
-              thickness="5"
-              color="black"
-              length="80"
-              class="border-opacity-100 p-0"
-              style="margin: 15px auto 30px auto"
-            >
-            </v-divider>
+                <p class="mb-4 md:mb-8 text-xs xl:text-sm 2xl:text-lg font-Poppins" v-for="desc in description">
+                  {{ desc }}
+                </p>
+                <nuxt-link to="/contact">
+                  <v-btn variant="text" style="padding: 0px">
+                    <span :class="getTitleColor(route.name)">Learn More</span>
+                    <v-icon right class="ml-2" :class="getTitleColor(route.name)">mdi-arrow-right</v-icon>
+                  </v-btn>
+                </nuxt-link>
+              </div>
+            </v-window-item>
+            <v-window-item value="products">
+              <div class="p-5 md:p-8" style="margin: 0px auto; padding-top: 1.5rem">
+                <!--<h1
+                  class="font-black font-Roboto text-center text-lg md:text-xl xl:text-2xl 2xl:text-4xl text-purple-900"
+                  :class="getTitleColor(route.name)"
+                >
+                  Products
+                </h1>
+                <v-divider
+                  thickness="5"
+                  color="black"
+                  length="80"
+                  class="border-opacity-100 p-0"
+                  style="margin: 15px auto 30px auto"
+                >
+                </v-divider>-->
 
-            <!-- PRODUCTS WEB VIEW -->
-            <div class="grid hidden-sm-and-down" :class="getColsCount()">
-              <!-- copying what's in the current website -->
-              <v-card flat class="mx-4 xl:mx-3" v-for="product in products">
-                <v-img
-                  :src="`/static/images/${product.image}`"
-                  :lazy-src="`/static/images/${product.image}`"
-                  class="floating-image"
-                ></v-img>
-                <v-card-text class="font-Poppins" style="padding: 25px 5px; font-size: 0.8em">
-                  <!-- had to use basic css, tailwind text-xs wasn't working -->
-                  <!-- md:text-xs 2xl:text-base -->
-                  {{ product.text }}
-                </v-card-text>
-              </v-card>
-            </div>
-
-            <!-- PRODUCTS MOBILE VIEW -->
-            <div class="grid hidden-md-and-up">
-              <!-- copying what's in the current website -->
-              <v-carousel cycle hide-delimiters touch show-arrows="hover" progress="grey-darken-1">
-                <v-carousel-item v-for="product in products">
-                  <v-card flat class="mx-4 xl:mx-3">
+                <!-- PRODUCTS WEB VIEW -->
+                <div class="grid hidden-sm-and-down" :class="getColsCount()">
+                  <!-- copying what's in the current website -->
+                  <v-card flat class="mx-4 xl:mx-3" v-for="product in products">
                     <v-img
                       :src="`/static/images/${product.image}`"
                       :lazy-src="`/static/images/${product.image}`"
                       class="floating-image"
                     ></v-img>
-                    <v-card-text class="text-xs font-Poppins" style="padding: 25px 0px">
+                    <v-card-text class="font-Poppins" style="padding: 25px 5px; font-size: 0.8em">
+                      <!-- had to use basic css, tailwind text-xs wasn't working -->
+                      <!-- md:text-xs 2xl:text-base -->
                       {{ product.text }}
                     </v-card-text>
                   </v-card>
-                </v-carousel-item>
-              </v-carousel>
-            </div>
-          </div>
-        </v-window-item>
-      </v-window>
+                </div>
+
+                <!-- PRODUCTS MOBILE VIEW -->
+                <div class="grid hidden-md-and-up">
+                  <!-- copying what's in the current website -->
+                  <v-carousel cycle hide-delimiters touch show-arrows="hover" progress="grey-darken-1">
+                    <v-carousel-item v-for="product in products">
+                      <v-card flat class="mx-4 xl:mx-3">
+                        <v-img
+                          :src="`/static/images/${product.image}`"
+                          :lazy-src="`/static/images/${product.image}`"
+                          class="floating-image"
+                        ></v-img>
+                        <v-card-text class="text-xs font-Poppins" style="padding: 25px 0px">
+                          {{ product.text }}
+                        </v-card-text>
+                      </v-card>
+                    </v-carousel-item>
+                  </v-carousel>
+                </div>
+              </div>
+            </v-window-item>
+          </v-window>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -134,14 +187,14 @@ let props = defineProps({
   },
 });
 
-const getInactiveClass = (partner) => {
+const getActiveClass = (partner) => {
   let pageColor = '';
   switch (partner) {
     case 'DataStax':
-      pageColor = 'inactiveDataStax';
+      pageColor = 'activeDataStax';
       break;
     case 'fastly':
-      pageColor = 'inactiveFastly';
+      pageColor = 'activeFastly';
       break;
   }
   return pageColor;
@@ -167,20 +220,18 @@ const getColsCount = () => {
 </script>
 
 <style scoped>
-.inactiveDataStax {
+.activeDataStax {
   background-color: #e1bee724;
   border-top: 2px #e1bee7b1 solid;
-  border-right: 2px #e1bee7b1 solid;
-  border-left: 2px #e1bee7b1 solid;
-  color: #e1bee7;
+  border-bottom: 2px #e1bee7b1 solid;
+  color: #80488a;
 }
 
-.inactiveFastly {
+.activeFastly {
   background-color: #e7bebe24;
   border-top: 2px #e7bebeb1 solid;
-  border-right: 2px #e7bebeb1 solid;
-  border-left: 2px #e7bebeb1 solid;
-  color: #e7bebe;
+  border-bottom: 2px #e7bebeb1 solid;
+  color: #8a4848;
 }
 
 .networkBG {
