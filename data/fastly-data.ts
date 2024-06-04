be75974b97a5c65257dcc67ec7de0e9f93345719
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-06-04 14:56:20 +0800

    utilized data files for partners

---
+++ data/fastly-data.ts
@@ -0,0 +1,27 @@
+export const tagline = 'Welcome to Our Fastly Partnership: Accelerate Your Digital Experience with Confidence';
+
+export const descriptionFastly = [
+  "We're thrilled to introduce our partnership with Fastly – the industry leader in content delivery, security, and edge computing. Together, we're committed to helping businesses like yours achieve unparalleled digital performance and security, all while delivering exceptional user experiences.",
+  "Fastly is an innovative cloud platform that enables businesses to deliver websites and applications swiftly and securely to global users. With a strategically positioned global server network, Fastly serves as a high-speed conduit for internet traffic, ensuring rapid and reliable access to web content. By leveraging this network, Fastly optimizes online content delivery, delivering a seamless and efficient browsing experience for users.",
+  "As your trusted partner, we're here to help you leverage the full potential of Fastly's platform. Whether you're looking to accelerate content delivery, fortify your security posture, or unlock the power of edge computing, our partnership with Fastly provides the tools and expertise you need to succeed in today's digital landscape.",
+  "Join us in shaping the future of digital delivery and security with Fastly – together, we'll accelerate your online success with confidence.",
+];
+
+export const productsFastly = [
+  {
+    image: 'CDN.png',
+    text: "Fastly's Content Delivery Network (CDN) revolutionizes the way businesses deliver online content to their audiences. With strategically positioned edge servers worldwide, Fastly's CDN ensures lightning-fast access to web content, optimizing user experiences regardless of location. Discover how Fastly's CDN empowers businesses to reach global audiences with unparalleled speed and reliability.",
+  },
+  {
+    image: 'computeedge.png',
+    text: "Fastly's Compute@Edge is reshaping the digital landscape by bringing computation closer to the user, enabling real-time processing and customization of content at the edge of the network. Experience the power of Compute@Edge as it unlocks endless possibilities for dynamic, personalized online experiences, setting new standards for speed, flexibility, and innovation.",
+  },
+  {
+    image: 'security.png',
+    text: "Fastly’s comprehensive security solutions offer protection against a wide range of threats, from DDoS attacks to data breaches. With real-time threat intelligence and robust security features, Fastly provides the peace of mind you need to focus on growing your business while we safeguard your online presence.",
+  },
+  {
+    image: 'next-gen_waf.png',
+    text: "Fastly's Next-Gen Web Application Firewall (WAF) is your frontline defense against sophisticated cyber threats targeting your web applications. With advanced threat detection capabilities and real-time updates, our WAF goes beyond traditional security measures to provide proactive protection for your online assets.",
+  },
+];
