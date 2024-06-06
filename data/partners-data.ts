30f9069ecf99910c23c544162e30dc11b6bd3361
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-06-06 18:27:48 +0800

    added partners data and dynamic partners pages

---
+++ data/partners-data.ts
@@ -0,0 +1,66 @@
+export const partnersData = [
+  {
+    name: 'DataStax',
+    link: 'datastax',
+    cover: 'datastax-cover.png',
+    background: 'datastax-bg-animation.mp4',
+    tagline: 'DataStax: A Trusted Partner for Cutting-edge Cloud Database Solutions',
+    description: [
+      "Step into the forefront of modern data management with DataStax, your premier partner for revolutionary distributed hybrid cloud database systems. Our suite of solutions, including DataStax Enterprise (DSE) and Astra, seamlessly merges Apache Cassandra's renowned scalability with enterprise-grade functionalities, empowering organizations with unparalleled capabilities in real-time data distribution and analytics.",
+      "Tailored consultancy, training, and support services are at your fingertips to address your specific needs. Whether you're initiating a new data project, integrating complex systems, refining existing infrastructure, or seeking optimization strategies to enhance performance, our expert team provides invaluable guidance and assistance at every phase.",
+      "With DataStax as your ally, you'll harness cutting-edge technology and receive unwavering support to ensure your success. From strategic planning to seamless implementation, we uphold our commitment to empower you to foster innovation throughout your organization. Explore how DataStax can revolutionize your data management, unlocking boundless opportunities for your business to thrive.",
+    ],
+    products: [
+      {
+        image: 'astra-db.png',
+        text: 'Introducing Astra DB: the groundbreaking, globally distributed, serverless, multi-model database service meticulously crafted by DataStax to cater to users across their preferred cloud platform.',
+      },
+      {
+        image: 'vector-search.png',
+        text: 'A cutting-edge technique revolutionizing the way we navigate and analyze large-scale datasets by representing data points as vectors and enabling advanced similarity searches for enhanced information retrieval and analysis.',
+      },
+      {
+        image: 'astra-streaming.png',
+        text: 'A state-of-the-art platform empowering businesses to harness real-time data streams for dynamic insights and actionable intelligence, revolutionizing decision-making processes and enhancing operational efficiency.',
+      },
+      {
+        image: 'ragstack.png',
+        text: 'A comprehensive platform designed to streamline data management and analysis processes, empowering businesses to efficiently organize, analyze, and visualize their data for actionable insights and informed decision-making.',
+      },
+      {
+        image: 'datastax-enterprise.png',
+        text: 'The leading distributed hybrid cloud database platform, engineered to empower organizations with unparalleled scalability, performance, and reliability for mission-critical applications and real-time data-driven insights.',
+      },
+    ],
+  },
+  {
+    name: 'Fastly',
+    link: 'fastly',
+    cover: 'fastly-cover.png',
+    background: 'fastly-bg-animation.mp4',
+    tagline: 'Welcome to Our Fastly Partnership: Accelerate Your Digital Experience with Confidence',
+    description: [
+      "We've partnered with Fastly, a leader in content delivery, security, and edge computing, to revolutionize digital performance and security for enterprises like yours. Together, we guarantee unparalleled user experiences and operational excellence in today's competitive business landscape.",
+      "Fastly's cloud platform ensures seamless delivery of websites and applications globally. With a global server network, Fastly provides swift and dependable access to web content, enhancing user satisfaction and operational efficiency. As your ally, we're here to help you leverage Fastly's capabilities, whether it's expediting content delivery, fortifying security, or exploring edge computing opportunities.",
+      "Furthermore, our collaborative approach extends beyond implementation, as we provide ongoing support and strategic guidance to optimize your digital infrastructure. With Fastly's cutting-edge solutions and our expertise, you can navigate the complexities of the digital realm with confidence, unlocking new possibilities for growth and innovation.",
+    ],
+    products: [
+      {
+        image: 'CDN.png',
+        text: "Fastly's Content Delivery Network (CDN) revolutionizes the way businesses deliver online content to their audiences. With strategically positioned edge servers worldwide, Fastly's CDN ensures lightning-fast access to web content, optimizing user experiences regardless of location. Discover how Fastly's CDN empowers businesses to reach global audiences with unparalleled speed and reliability.",
+      },
+      {
+        image: 'computeedge.png',
+        text: "Fastly's Compute@Edge is reshaping the digital landscape by bringing computation closer to the user, enabling real-time processing and customization of content at the edge of the network. Experience the power of Compute@Edge as it unlocks endless possibilities for dynamic, personalized online experiences, setting new standards for speed, flexibility, and innovation.",
+      },
+      {
+        image: 'security.png',
+        text: 'Fastly’s comprehensive security solutions offer protection against a wide range of threats, from DDoS attacks to data breaches. With real-time threat intelligence and robust security features, Fastly provides the peace of mind you need to focus on growing your business while we safeguard your online presence.',
+      },
+      {
+        image: 'next-gen_waf.png',
+        text: "Fastly's Next-Gen Web Application Firewall (WAF) is your frontline defense against sophisticated cyber threats targeting your web applications. With advanced threat detection capabilities and real-time updates, our WAF goes beyond traditional security measures to provide proactive protection for your online assets.",
+      },
+    ],
+  },
+];
