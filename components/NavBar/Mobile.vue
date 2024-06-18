34d2233a7cca744e156b0b4521411ed582e725a3
NathanielGabrielSantos <gabesantos726@gmail.com>
2024-06-18 23:35:02 +0800

    Added special styling for event buttons and applied active-link on mobile navbar when on certain pages

--- components/NavBar/Mobile.vue
+++ components/NavBar/Mobile.vue
@@ -17,19 +17,36 @@
           <template v-for="item in navItems">
             <template v-if="item.type === 'page' && item.enabled">
               <v-list-item>
-                <NuxtLink :to="item.link" class="cursor-pointer p-3 text-hover">{{ item.title }}</NuxtLink>
+                <NuxtLink
+                  :to="item.link"
+                  class="cursor-pointer p-3 hover:text-[#ff7b02]"
+                  :class="{ 'active-link': route.path === item.link, 'special-link': !item.common }"
+                  >{{ item.title }}</NuxtLink
+                >
               </v-list-item>
             </template>
             <template v-if="item.type === 'dropdown' && item.enabled">
               <v-list-group :value="item.title" class="px-3">
                 <template v-slot:activator="{ props }">
-                  <v-list-item v-bind="props" :title="item.title"></v-list-item>
+                  <v-list-item v-bind="props">
+                    <NuxtLink
+                      class="cursor-pointer hover:text-[#ff7b02]"
+                      :class="{
+                        'active-link': Object.keys(route.params)[0] === item.routeParamName,
+                        'special-link': !item.common,
+                      }"
+                      >{{ item.title }}</NuxtLink
+                    >
+                  </v-list-item>
                 </template>
                 <v-list-item
                   v-for="subItem in item.props"
                   :key="subItem.title"
                   :title="subItem.title"
-                  class="text-hover"
+                  class="hover:text-[#ff7b02]"
+                  :class="{
+                    'active-link': item.nested && `/${Object.keys(route.params)}s/${subItem.link}` === route.path,
+                  }"
                   @click="redirect(`${item.nested ? `/${item.component}/` : '/'}${subItem.link}`)"
                 >
                   <template v-slot:prepend>
@@ -55,8 +72,9 @@
   </div>
 </template>
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 
+const route = useRoute();
 const router = useRouter();
 
 const props = defineProps({
@@ -70,3 +88,10 @@ const redirect = (link) => {
   router.push(link);
 };
 </script>
+
+<style scoped>
+/* Overriden default padding of vuetify list item */
+.v-list-group__items .v-list-item {
+  padding-inline-start: 10% !important;
+}
+</style>
